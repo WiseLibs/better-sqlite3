712fd2c7ae7d65fd12a90dc0c771451661e3192f
Joshua Wise <josh@joshuawise.ninja>
2016-03-13 06:28:27 -0400

    added column plucking

--- src/statement.cc
+++ src/statement.cc
@@ -1,5 +1,6 @@
+#include <cstdio>
 #include <cstdlib>
-#include <cmath>
+#include <cstring>
 #include <sqlite3.h>
 #include <nan.h>
 #include "macros.h"
@@ -34,10 +35,11 @@ class RunWorker : public StatementWorker {
 
 class GetWorker : public StatementWorker {
 	public:
-		GetWorker(Statement*, sqlite3_stmt*, int);
+		GetWorker(Statement*, sqlite3_stmt*, int, int);
 		void Execute();
 		void HandleOKCallback();
 	private:
+		int pluck_column;
 		Data::Row row;
 };
 
@@ -48,6 +50,7 @@ class GetWorker : public StatementWorker {
 Statement::Statement() : Nan::ObjectWrap(),
 	db(NULL),
 	source_string(NULL),
+	pluck_column(-1),
 	closed(false),
 	handles(NULL),
 	handle_states(NULL),
@@ -70,6 +73,7 @@ void Statement::Init() {
 	t->SetClassName(Nan::New("Statement").ToLocalChecked());
 	
 	Nan::SetPrototypeMethod(t, "cache", Cache);
+	Nan::SetPrototypeMethod(t, "pluck", Pluck);
 	Nan::SetPrototypeMethod(t, "run", Run);
 	Nan::SetPrototypeMethod(t, "get", Get);
 	Nan::SetAccessor(t->InstanceTemplate(), Nan::New("readonly").ToLocalChecked(), ReadonlyGetter);
@@ -96,12 +100,15 @@ NAN_METHOD(Statement::Cache) {
 		return Nan::ThrowError("The associated database connection is closed.");
 	}
 	if (stmt->cache_locked) {
-		return Nan::ThrowError("A Statement's cache cannot be altered after it has been executed.");
+		return Nan::ThrowError("A statement's cache cannot be altered after it has been executed.");
 	}
 	
 	double numberValue = number->Value();
-	if (!std::isfinite(numberValue) || numberValue < 1) {
-		return Nan::ThrowError("Argument 0 must be a positive, finite number.");
+	if (!IS_POSITIVE_INTEGER(numberValue)) {
+		return Nan::ThrowError("Argument 0 must be a positive, finite integer.");
+	}
+	if (numberValue < 1) {
+		numberValue = 1;
 	}
 	
 	int len = (int)numberValue;
@@ -126,19 +133,75 @@ NAN_METHOD(Statement::Cache) {
 	
 	info.GetReturnValue().Set(info.This());
 }
+NAN_METHOD(Statement::Pluck) {
+	Statement* stmt = Nan::ObjectWrap::Unwrap<Statement>(info.This());
+	if (!stmt->readonly) {
+		return Nan::ThrowTypeError("The pluck() method can only be used by read-only statements.");
+	}
+	if (stmt->db->state == DB_DONE) {
+		return info.GetReturnValue().Set(info.This());
+	}
+	REQUIRE_ARGUMENTS(1);
+	v8::Local<v8::Value> arg = info[0];
+	
+	if (arg->IsFalse()) {
+		stmt->pluck_column = -1;
+	} else {
+		sqlite3_stmt* handle = stmt->handles[0];
+		int column_count = sqlite3_column_count(handle);
+		
+		if (arg->IsTrue()) {
+			if (column_count < 1) {return Nan::ThrowTypeError("This statement does not return any result columns.");}
+			stmt->pluck_column = 0;
+		} else if (arg->IsNumber()) {
+			double num = v8::Local<v8::Number>::Cast(arg)->Value();
+			if (!IS_POSITIVE_INTEGER(num)) {
+				return Nan::ThrowTypeError("Column numbers must be finite, positive integers.");
+			}
+			if (column_count <= num) {
+				const char format[] = "This statement only returns %d result columns.";
+				char buffer[sizeof(format) + 64];
+				int len = sprintf(buffer, format, column_count);
+				return Nan::ThrowTypeError(Nan::New<v8::String>(buffer, len).ToLocalChecked());
+			}
+			stmt->pluck_column = (int)num;
+		} else if (arg->IsString()) {
+			Nan::Utf8String utf8(v8::Local<v8::String>::Cast(arg));
+			const char* str = *utf8;
+			int i = 0;
+			for (; i<column_count; i++) {
+				const char* name = sqlite3_column_name(handle, i);
+				if (strcmp(name, str) == 0) {
+					stmt->pluck_column = i;
+					break;
+				}
+			}
+			if (i == column_count) {
+				return Nan::ThrowTypeError("The specified column name is not returned by this statement.");
+			}
+		} else {
+			return Nan::ThrowTypeError("You must specify true, false, a column number, or a column name.");
+		}
+	}
+	info.GetReturnValue().Set(info.This());
+}
 NAN_METHOD(Statement::Run) {
 	Statement* stmt = Nan::ObjectWrap::Unwrap<Statement>(info.This());
 	if (stmt->readonly) {
-		return Nan::ThrowTypeError("This Statement is read-only. Use get(), all(), or each() instead.");
+		return Nan::ThrowTypeError("This statement is read-only. Use get(), all(), or each() instead.");
 	}
-	STATEMENT_START(stmt, RunWorker);
+	STATEMENT_START(stmt);
+	RunWorker* worker = new RunWorker(stmt, _handle, _i);
+	STATEMENT_END(stmt, worker);
 }
 NAN_METHOD(Statement::Get) {
 	Statement* stmt = Nan::ObjectWrap::Unwrap<Statement>(info.This());
 	if (!stmt->readonly) {
-		return Nan::ThrowTypeError("This Statement is not read-only. Use run() instead.");
+		return Nan::ThrowTypeError("This statement is not read-only. Use run() instead.");
 	}
-	STATEMENT_START(stmt, GetWorker);
+	STATEMENT_START(stmt);
+	GetWorker* worker = new GetWorker(stmt, _handle, _i, stmt->pluck_column);
+	STATEMENT_END(stmt, worker);
 }
 void Statement::CloseStatement(Statement* stmt) {
 	stmt->closed = true;
@@ -224,14 +287,26 @@ void RunWorker::HandleOKCallback() {
 
 
 
-GetWorker::GetWorker(Statement* stmt, sqlite3_stmt* handle, int handle_index)
-	: StatementWorker(stmt, handle, handle_index) {}
+GetWorker::GetWorker(Statement* stmt, sqlite3_stmt* handle, int handle_index, int pluck_column)
+	: StatementWorker(stmt, handle, handle_index), pluck_column(pluck_column) {}
 void GetWorker::Execute() {
 	int status = sqlite3_step(handle);
 	if (status == SQLITE_ROW) {
+		int i;
 		int len = sqlite3_column_count(handle);
-		row.Init(len);
-		for (int i=0; i<len; i++) {
+		if (pluck_column >= 0) {
+			if (pluck_column < len) {
+				i = pluck_column;
+				len = pluck_column + 1;
+				row.Init(1);
+			} else {
+				return SetErrorMessage("The plucked column no longer exists.");
+			}
+		} else {
+			i = 0;
+			row.Init(len);
+		}
+		for (; i<len; i++) {
 			int type = sqlite3_column_type(handle, i);
 			switch (type) {
 				case SQLITE_INTEGER:
@@ -250,8 +325,7 @@ void GetWorker::Execute() {
 					row.Add(new Data::Null());
 					break;
 				default:
-					SetErrorMessage("SQLite returned an unrecognized data type.");
-					return;
+					return SetErrorMessage("SQLite returned an unrecognized data type.");
 			}
 		}
 	} else if (status != SQLITE_DONE) {
@@ -260,6 +334,10 @@ void GetWorker::Execute() {
 }
 void GetWorker::HandleOKCallback() {
 	Nan::HandleScope scope;
+	
+	if (pluck_column >= 0) {
+		return Resolve(row.values[0]->ToJS());
+	}
 	
 	v8::Local<v8::Object> obj = Nan::New<v8::Object>();
 	for (int i=0; i<row.column_count; i++) {
