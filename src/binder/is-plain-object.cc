5c40df93411f521f307dffd5193ca38c5eec67bc
Joshua Wise <jwise@hearst.com>
2016-09-01 16:58:31 -0400

    fixed IsPlainObject

--- src/binder/is-plain-object.cc
+++ src/binder/is-plain-object.cc
@@ -2,6 +2,9 @@
 
 bool Binder::IsPlainObject(v8::Local<v8::Object> obj) {
 	v8::Local<v8::Value> proto = obj->GetPrototype();
+	v8::Local<v8::Context> ctx = obj->CreationContext();
+	ctx->Enter();
 	v8::Local<v8::Value> baseProto = Nan::New<v8::Object>()->GetPrototype();
+	ctx->Exit();
 	return proto->StrictEquals(baseProto) || proto->StrictEquals(Nan::Null());
 }
