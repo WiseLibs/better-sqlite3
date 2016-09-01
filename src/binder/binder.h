3c05a20d1980bcbb63a035431900e240aff53661
Joshua Wise <jwise@hearst.com>
2016-09-01 12:03:15 -0400

    removed multibinder from binder

--- src/binder/binder.h
+++ src/binder/binder.h
@@ -6,11 +6,8 @@
 class Binder {
 	public:
 		Binder(sqlite3_stmt*);
-		Binder(sqlite3_stmt**, unsigned int);
 		~Binder();
-		// Either Bind or BindMulti should be used, NOT both, and only once
 		void Bind(Nan::NAN_METHOD_ARGS_TYPE, int);
-		void BindMulti(Nan::NAN_METHOD_ARGS_TYPE, int);
 		const char* GetError();
 		
 	private:
@@ -30,27 +27,13 @@ class Binder {
 		double GetArrayLikeLength(v8::Local<v8::Object>);
 		static bool IsPlainObject(v8::Local<v8::Object>);
 		
-		// For multi-handle usage
-		sqlite3_stmt** const handles;
-		unsigned int const handle_count;
-		
-		// For single-handle usage
-		sqlite3_stmt* const handle;
-		
-		unsigned int const param_count;
+		sqlite3_stmt* handle;
+		unsigned int param_count;
 		
 		unsigned int anon_index; // This value should only be used by NextAnonIndex()
 		const char* error;
 		char* error_extra;
 		const char* error_full;
-		
-		unsigned int CountParams() {
-			unsigned int total = 0;
-			for (unsigned int i=0; i<handle_count; ++i) {
-				total += sqlite3_bind_parameter_count(handles[i]);
-			}
-			return total;
-		}
 };
 
 #endif
