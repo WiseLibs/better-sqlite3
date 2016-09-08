29d72fa7602e6ffc61b4020ec20f408f409b6d22
Joshua Wise <joshuathomaswise@gmail.com>
2016-09-08 16:28:31 -0400

    added some tests

---
+++ test/11.database.pragma.js
@@ -0,0 +1,11 @@
+var expect = require('chai').expect;
+var Database = require('../.');
+
+describe('Database PRAGMA modification', function () {
+	describe('.pragma()', function () {
+		
+	});
+	describe('"wal" option', function () {
+		
+	});
+});
---
+++ test/12.database.close.js
@@ -0,0 +1,14 @@
+var expect = require('chai').expect;
+var Database = require('../.');
+
+describe('Closing a database', function () {
+	describe('before opening', function () {
+		
+	});
+	describe('after opening', function () {
+		
+	});
+	describe('because of a failed open attempt', function () {
+		
+	});
+});
