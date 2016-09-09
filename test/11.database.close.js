81931f55ae215869136d6818e34caf58f9a19cb1
Joshua Wise <jwise@hearst.com>
2016-09-09 16:15:36 -0400

    fixed tests

--- test/11.database.close.js
+++ test/11.database.close.js
@@ -1,10 +1,10 @@
 var expect = require('chai').expect;
 var Database = require('../.');
 
-describe('Closing a database', function () {
+describe('Database#close()', function () {
 	describe('before opening it', function () {
 		it('should never emit the "open" event', function (done) {
-			var db = new Database('temp/12.1.db');
+			var db = new Database('temp/11.1.db');
 			expect(db.open).to.be.false;
 			db.on('open', function () {
 				done(new Error('This event should not have been fired.'));
@@ -20,7 +20,7 @@ describe('Closing a database', function () {
 	});
 	describe('because of a failed open attempt', function (done) {
 		it('should have an Error object as its first parameter', function () {
-			var db = new Database('temp/nonexistent/abcfoobar123/12.2.db');
+			var db = new Database('temp/nonexistent/abcfoobar123/11.2.db');
 			db.on('open', function () {
 				done(new Error('This event should not have been fired.'));
 			});
@@ -33,7 +33,7 @@ describe('Closing a database', function () {
 	});
 	describe('after opening it', function () {
 		it('should emit the close event as normal', function (done) {
-			var db = new Database('temp/12.3.db');
+			var db = new Database('temp/11.3.db');
 			expect(db.open).to.be.false;
 			db.on('open', function () {
 				expect(db.open).to.be.true;
@@ -42,19 +42,6 @@ describe('Closing a database', function () {
 				db.on('close', function (err) {
 					expect(err).to.be.null;
 					expect(db.open).to.be.false;
-					done();
-				});
-			});
-		});
-		it('should prevent further PRAGMA executions', function (done) {
-			var db = new Database('temp/12.4.db');
-			db.on('open', function () {
-				expect(db.pragma('cache_size', true)).to.equal('-16000');
-				db.close();
-				expect(function () {db.pragma('cache_size', true)}).to.throw(Error);
-				db.on('close', function (err) {
-					expect(err).to.be.null;
-					expect(function () {db.pragma('cache_size', true)}).to.throw(Error);
 					done();
 				});
 			});
