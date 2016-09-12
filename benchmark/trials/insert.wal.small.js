8c2ad6e94c7c4ee3a061dd2e7d7d36314603f900
Joshua Wise <joshuathomaswise@gmail.com>
2016-09-12 03:44:36 -0400

    improved benchmark

--- benchmark/trials/insert.wal.small.js
+++ benchmark/trials/insert.wal.small.js
@@ -3,7 +3,7 @@ require('../get-db')('insert-db', function (ourDb, theirDb) {
 	ourDb.pragma('journal_mode = WAL');
 	ourDb.pragma('synchronous = 1');
 	theirDb.exec('PRAGMA journal_mode = WAL; PRAGMA synchronous = 1;', function () {
-		require('../insert-trial')(ourDb, theirDb, 500, [
+		require('../insert-trial')(ourDb, theirDb, 5000, [
 			'John Smith',
 			123456,
 			null
