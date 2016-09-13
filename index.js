48057a8d8076d81c66a3b3d74fd5fb5f470cc51f
Joshua Wise <joshuathomaswise@gmail.com>
2016-09-13 05:04:15 -0400

    added helpers to Int64

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 'use strict';
 module.exports = require('./lib/database');
-module.exports.Int64 = require('bindings')('better_sqlite3').Int64;
+var Int64 = module.exports.Int64 = require('bindings')('better_sqlite3').Int64;
+
+defineFrozen(Int64, 'MAX_VALUE', new Int64(0xffffffff, 0x7fffffff));
+defineFrozen(Int64, 'MIN_VALUE', new Int64(0, 0x80000000));
+defineFrozen(Int64, 'ZERO', new Int64(0, 0));
+defineFrozen(Int64, 'ONE', new Int64(1, 0));
+defineFrozen(Int64, 'NEG_ONE', new Int64(0xffffffff, 0xffffffff));
+
+Int64.prototype.equals = function (other) {
+	if (!(other instanceof Int64)) {
+		return +this == other;
+	}
+	return this.low === other.low && this.high === other.high;
+};
+
+function defineFrozen(obj, key, value) {
+	Object.defineProperty(obj, key, {
+		writable: false,
+		enumerable: true,
+		configurable: false,
+		value: value
+	});
+}
