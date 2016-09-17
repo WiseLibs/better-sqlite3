50b561f84bf95d2d357b748f304773b0c1939cfa
Joshua Wise <joshuathomaswise@gmail.com>
2016-09-17 12:30:57 -0400

    exposed the "memory" property on database objects

--- src/objects/database/new.cc
+++ src/objects/database/new.cc
@@ -3,9 +3,11 @@
 NAN_METHOD(Database::New) {
 	REQUIRE_ARGUMENT_STRING(0, filename);
 	REQUIRE_ARGUMENT_STRING(1, filenameGiven);
+	REQUIRE_ARGUMENT_BOOLEAN(2, inMemory);
 	
 	Database* db = new Database();
 	db->Wrap(info.This());
+	Nan::ForceSet(info.This(), NEW_INTERNAL_STRING_FAST("memory"), inMemory ? Nan::True() : Nan::False(), FROZEN);
 	Nan::ForceSet(info.This(), NEW_INTERNAL_STRING_FAST("name"), filenameGiven, FROZEN);
 	
 	db->Ref();
