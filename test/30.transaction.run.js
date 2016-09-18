abd1c3f52f8d3f81ab46396ea6c9b375c28e1c75
Joshua Wise <joshuathomaswise@gmail.com>
2016-09-17 22:06:22 -0400

    finished transaction.bind tests

--- test/30.transaction.run.js
+++ test/30.transaction.run.js
@@ -85,65 +85,103 @@ describe('Transaction#run()', function () {
 		var trans = db.transaction(["DROP TABLE people"]);
 		expect(trans.run().changes).to.equal(2);
 	});
-	// it('should accept bind parameters', function () {
-	// 	db.transaction("CREATE TABLE entries (a TEXT CHECK(typeof(a)=='text'), b INTEGER CHECK(typeof(b)=='integer' OR typeof(b)=='real'), c REAL CHECK(typeof(c)=='real' OR typeof(c)=='integer'), d BLOB CHECK(typeof(d)=='blob'))").run();
-	// 	db.transaction('INSERT INTO entries VALUES (?, ?, ?, ?)').run('foo', 25, 25, Buffer.alloc(8).fill(0xdd));
-	// 	db.transaction('INSERT INTO entries VALUES (?, ?, ?, ?)').run(['foo', 25, 25, Buffer.alloc(8).fill(0xdd)]);
-	// 	db.transaction('INSERT INTO entries VALUES (?, ?, ?, ?)').run(['foo', 25], [25], Buffer.alloc(8).fill(0xdd));
-	// 	db.transaction('INSERT INTO entries VALUES (@a, @b, @c, @d)').run({a: 'foo', b: 25, c: 25, d: Buffer.alloc(8).fill(0xdd)});
-	// 	db.transaction('INSERT INTO entries VALUES ($a, $b, $c, $d)').run({a: 'foo', b: 25, c: 25, d: Buffer.alloc(8).fill(0xdd)});
-	// 	db.transaction('INSERT INTO entries VALUES (:a, :b, :c, :d)').run({a: 'foo', b: 25, c: 25, d: Buffer.alloc(8).fill(0xdd)});
-	// 	db.transaction('INSERT INTO entries VALUES (?, @a, @a, ?)').run({a: 25}, ['foo'], Buffer.alloc(8).fill(0xdd));
-	// 	expect(function () {
-	// 		db.transaction('INSERT INTO entries VALUES (?, @a, @a, ?)').run({a: 25}, ['foo'], Buffer.alloc(8).fill(0xdd), Buffer.alloc(8).fill(0xdd));
-	// 	}).to.throw(Error);
-	// 	expect(function () {
-	// 		db.transaction('INSERT INTO entries VALUES (?, @a, @a, ?)').run({a: 25}, ['foo']);
-	// 	}).to.throw(Error);
-	// 	expect(function () {
-	// 		db.transaction('INSERT INTO entries VALUES (?, @a, @a, ?)').run({a: 25, c: 25}, ['foo'], Buffer.alloc(8).fill(0xdd));
-	// 	}).to.throw(Error);
-	// 	expect(function () {
-	// 		db.transaction('INSERT INTO entries VALUES (?, @a, @a, ?)').run({}, ['foo'], Buffer.alloc(8).fill(0xdd));
-	// 	}).to.throw(Error);
-	// 	expect(function () {
-	// 		db.transaction('INSERT INTO entries VALUES (?, ?, ?, ?)').run(25, 'foo', 25, Buffer.alloc(8).fill(0xdd));
-	// 	}).to.throw(Error);
-	// 	expect(function () {
-	// 		db.transaction('INSERT INTO entries VALUES (?, ?, ?, ?)').run('foo', 25, 25, Buffer.alloc(8).fill(0xdd), {foo: 'foo'});
-	// 	}).to.throw(Error);
-	// 	db.transaction('INSERT INTO entries VALUES (?, ?, ?, ?)').run('foo', 25, 25, Buffer.alloc(8).fill(0xdd), {});
-	// 	expect(function () {
-	// 		db.transaction('INSERT INTO entries VALUES (?, ?, ?, ?)').run('foo', 25, 25, {4: Buffer.alloc(8).fill(0xdd)});
-	// 	}).to.throw(Error);
-	// 	expect(function () {
-	// 		db.transaction('INSERT INTO entries VALUES (?, ?, ?, ?)').run();
-	// 	}).to.throw(Error);
-	// 	expect(function () {
-	// 		db.transaction('INSERT INTO entries VALUES (?, ?, ?, ?)').run({length: 4, 0: 'foo', 1: 25, 2: 25, 3: Buffer.alloc(8).fill(0xdd)});
-	// 	}).to.throw(Error);
-	// 	expect(function () {
-	// 		db.transaction('INSERT INTO entries VALUES (?, ?, ?, ?)').run('foo', 25, new Number(25), Buffer.alloc(8).fill(0xdd));
-	// 	}).to.throw(Error);
-	// 	expect(function () {
-	// 		db.transaction('INSERT INTO entries VALUES (?, ?, ?, ?)').run('foo', {low: 25, high: 25}, 25, Buffer.alloc(8).fill(0xdd));
-	// 	}).to.throw(Error);
-	// 	function Foo() {
-	// 		this.a = 'foo';
-	// 		this.b = 25;
-	// 		this.c = 25;
-	// 		this.d = Buffer.alloc(8).fill(0xdd);
-	// 	}
-	// 	expect(function () {
-	// 		db.transaction('INSERT INTO entries VALUES (@a, @b, @c, @d)').run(new Foo);
-	// 	}).to.throw(Error);
+	it('should accept bind parameters', function () {
+		db.transaction(["CREATE TABLE entries (a TEXT CHECK(typeof(a)=='text'), b INTEGER CHECK(typeof(b)=='integer' OR typeof(b)=='real'), c REAL CHECK(typeof(c)=='real' OR typeof(c)=='integer'), d BLOB CHECK(typeof(d)=='blob'))"]).run();
 		
-	// 	// This part of the test may fail is Statement#get() does not work.
-	// 	var i = 0;
-	// 	var row;
-	// 	while (row = db.transaction('SELECT * FROM entries WHERE rowid=' + ++i).get()) {
-	// 		expect(row).to.deep.equal({a: 'foo', b: 25, c: 25, d: Buffer.alloc(8).fill(0xdd)})
-	// 	}
-	// 	expect(i).to.equal(9);
-	// });
+		db.transaction(['INSERT INTO entries VALUES (?, ?, ?, ?)', 'INSERT INTO entries VALUES (?, ?, ?, ?)'])
+			.run('foo', 25, 25, Buffer.alloc(8).fill(0xdd), 'foo', 25, 25, Buffer.alloc(8).fill(0xdd));
+			
+		db.transaction(['INSERT INTO entries VALUES (?, ?, ?, ?)', 'INSERT INTO entries VALUES (?, ?, ?, ?)'])
+			.run(['foo', 25, 25, Buffer.alloc(8).fill(0xdd), 'foo', 25, 25, Buffer.alloc(8).fill(0xdd)]);
+			
+		db.transaction(['INSERT INTO entries VALUES (?, ?, ?, ?)', 'INSERT INTO entries VALUES (?, ?, ?, ?)'])
+			.run(['foo', 25], [25], Buffer.alloc(8).fill(0xdd), ['foo', 25, 25, Buffer.alloc(8).fill(0xdd)]);
+			
+		db.transaction(['INSERT INTO entries VALUES (@a, @b, @c, @d)', 'INSERT INTO entries VALUES (@a, @b, @c, @d)'])
+			.run({a: 'foo', b: 25, c: 25, d: Buffer.alloc(8).fill(0xdd)});
+			
+		db.transaction(['INSERT INTO entries VALUES ($a, $b, $c, $d)', 'INSERT INTO entries VALUES ($a, $b, $c, $d)'])
+			.run({a: 'foo', b: 25, c: 25, d: Buffer.alloc(8).fill(0xdd)});
+			
+		db.transaction(['INSERT INTO entries VALUES (:a, :b, :c, :d)', 'INSERT INTO entries VALUES (:a, :b, :c, :d)'])
+			.run({a: 'foo', b: 25, c: 25, d: Buffer.alloc(8).fill(0xdd)});
+			
+		db.transaction(['INSERT INTO entries VALUES (?, @a, @a, ?)', 'INSERT INTO entries VALUES (?, @a, @a, ?)'])
+			.run({a: 25}, ['foo'], [Buffer.alloc(8).fill(0xdd), 'foo'], Buffer.alloc(8).fill(0xdd));
+		
+		expect(function () {
+			db.transaction(['INSERT INTO entries VALUES (?, @a, @a, ?)', 'INSERT INTO entries VALUES (?, @a, @a, ?)'])
+				.run({a: 25}, ['foo'], Buffer.alloc(8).fill(0xdd), 'foo', Buffer.alloc(8).fill(0xdd), Buffer.alloc(8).fill(0xdd));
+		}).to.throw(Error);
+		
+		expect(function () {
+			db.transaction(['INSERT INTO entries VALUES (?, @a, @a, ?)', 'INSERT INTO entries VALUES (?, @a, @a, ?)'])
+				.run({a: 25}, ['foo'], Buffer.alloc(8).fill(0xdd), 'foo');
+		}).to.throw(Error);
+		
+		expect(function () {
+			db.transaction(['INSERT INTO entries VALUES (?, @a, @a, ?)', 'INSERT INTO entries VALUES (?, @a, @a, ?)'])
+				.run({a: 25, c: 25}, ['foo'], Buffer.alloc(8).fill(0xdd), ['foo'], Buffer.alloc(8).fill(0xdd));
+		}).to.throw(Error);
+		
+		expect(function () {
+			db.transaction(['INSERT INTO entries VALUES (?, @a, @a, ?)'])
+				.run({}, ['foo'], Buffer.alloc(8).fill(0xdd), ['foo'], Buffer.alloc(8).fill(0xdd));
+		}).to.throw(Error);
+		
+		expect(function () {
+			db.transaction(['INSERT INTO entries VALUES (?, ?, ?, ?)', 'INSERT INTO entries VALUES (?, ?, ?, ?)'])
+				.run(25, 'foo', 25, Buffer.alloc(8).fill(0xdd), 'foo', 25, 25, Buffer.alloc(8).fill(0xdd));
+		}).to.throw(Error);
+		
+		expect(function () {
+			db.transaction(['INSERT INTO entries VALUES (?, ?, ?, ?)', 'INSERT INTO entries VALUES (?, ?, ?, ?)'])
+				.run('foo', 25, 25, Buffer.alloc(8).fill(0xdd), 'foo', 25, 25, Buffer.alloc(8).fill(0xdd), {foo: 'foo'});
+		}).to.throw(Error);
+		
+		db.transaction(['INSERT INTO entries VALUES (?, ?, ?, ?)', 'INSERT INTO entries VALUES (?, ?, ?, ?)'])
+			.run('foo', 25, 25, Buffer.alloc(8).fill(0xdd), 'foo', 25, 25, Buffer.alloc(8).fill(0xdd), {});
+		
+		expect(function () {
+			db.transaction(['INSERT INTO entries VALUES (?, ?, ?, ?)'])
+				.run('foo', 25, 25, {4: Buffer.alloc(8).fill(0xdd)});
+		}).to.throw(Error);
+		
+		expect(function () {
+			db.transaction(['INSERT INTO entries VALUES (?, ?, ?, ?)'])
+				.run();
+		}).to.throw(Error);
+		
+		expect(function () {
+			db.transaction(['INSERT INTO entries VALUES (?, ?, ?, ?)'])
+				.run({length: 4, 0: 'foo', 1: 25, 2: 25, 3: Buffer.alloc(8).fill(0xdd)});
+		}).to.throw(Error);
+		
+		expect(function () {
+			db.transaction(['INSERT INTO entries VALUES (?, ?, ?, ?)'])
+				.run('foo', 25, new Number(25), Buffer.alloc(8).fill(0xdd));
+		}).to.throw(Error);
+		
+		expect(function () {
+			db.transaction(['INSERT INTO entries VALUES (?, ?, ?, ?)'])
+				.run('foo', {low: 25, high: 25}, 25, Buffer.alloc(8).fill(0xdd));
+		}).to.throw(Error);
+		
+		function Foo() {
+			this.a = 'foo';
+			this.b = 25;
+			this.c = 25;
+			this.d = Buffer.alloc(8).fill(0xdd);
+		}
+		expect(function () {
+			db.transaction(['INSERT INTO entries VALUES (@a, @b, @c, @d)', 'INSERT INTO entries VALUES (@a, @b, @c, @d)']).run(new Foo);
+		}).to.throw(Error);
+		
+		var i = 0;
+		var row;
+		while (row = db.prepare('SELECT * FROM entries WHERE rowid=' + ++i).get()) {
+			expect(row).to.deep.equal({a: 'foo', b: 25, c: 25, d: Buffer.alloc(8).fill(0xdd)})
+		}
+		expect(i).to.equal(17);
+	});
 });
