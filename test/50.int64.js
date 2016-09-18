f5b6962bb6ba67b03be7d13aa7487784f6c6275a
Joshua Wise <joshuathomaswise@gmail.com>
2016-09-18 00:25:31 -0400

    finished writing tests

--- test/50.int64.js
+++ test/50.int64.js
@@ -10,7 +10,13 @@ before(function (done) {
 	var openCount = 0;
 	db.on('open', opened);
 	db2.on('open', opened);
-	function opened() {++openCount === 2 && done();}
+	function opened() {
+		if (++openCount === 2) {
+			db.prepare('CREATE TABLE entries (a INTEGER, b REAL, c TEXT)').run();
+			db2.prepare('CREATE TABLE entries (a INTEGER, b REAL, c TEXT)').run();
+			done();
+		}
+	}
 });
 
 describe('Int64', function () {
@@ -75,10 +81,88 @@ describe('Int64', function () {
 		expect(int.equals(+String(int))).to.be.false;
 		expect(int.equals(+int)).to.be.false;
 	});
-	it('should bind to statements and transactions');
-	it('should get returned by operations after setting .safeIntegers()');
-	it('should react to changing settings inside an .each() callback');
-	it('should be safe from other databases inside an .each() callback');
-	it('should be able to change the default setting on the database');
-	it('should return fully accurate round-trip results');
+	it('should bind to statements and transactions', function () {
+		var int = new Int64(4243423, 234234234);
+		db.prepare('INSERT INTO entries VALUES (?, ?, ?)').run(int, int, int);
+		db.transaction(['INSERT INTO entries VALUES (?, ?, ?)']).run(int, int, int);
+		db.prepare('INSERT INTO entries VALUES (?, ?, ?)').bind(int, int, int).run();
+		db.transaction(['INSERT INTO entries VALUES (?, ?, ?)']).bind(int, int, int).run();
+		
+		db2.prepare('INSERT INTO entries VALUES (?, ?, ?)').run(int, int, int);
+		db2.transaction(['INSERT INTO entries VALUES (?, ?, ?)']).run(int, int, int);
+		db2.prepare('INSERT INTO entries VALUES (?, ?, ?)').bind(int, int, int).run();
+		db2.transaction(['INSERT INTO entries VALUES (?, ?, ?)']).bind(int, int, int).run();
+	});
+	it('should get returned by operations after setting .safeIntegers()', function () {
+		var int = new Int64(4243423, 234234234);
+		var stmt = db.prepare('SELECT a FROM entries').pluck();
+		expect(stmt.get()).to.equal(1006028374637854700);
+		expect(stmt.safeIntegers().get()).to.deep.equal(int);
+		expect(stmt.get()).to.deep.equal(int);
+		expect(stmt.safeIntegers(false).get()).to.equal(1006028374637854700);
+		expect(stmt.get()).to.equal(1006028374637854700);
+		expect(stmt.safeIntegers(true).get()).to.deep.equal(int);
+		expect(stmt.get()).to.deep.equal(int);
+		
+		stmt = db.prepare('SELECT b FROM entries').pluck();
+		expect(stmt.get()).to.equal(1006028374637854700);
+		expect(stmt.safeIntegers().get()).to.equal(1006028374637854700);
+		
+		stmt = db.prepare('SELECT c FROM entries').pluck();
+		expect(stmt.get()).to.equal('1006028374637854687');
+		expect(stmt.safeIntegers().get()).to.equal('1006028374637854687');
+	});
+	it('should react to changing settings inside an .each() callback', function () {
+		var int = new Int64(4243423, 234234234);
+		var stmt = db.prepare('SELECT * FROM entries');
+		var count = 0;
+		stmt.each(function (row) {
+			expect(row.b).to.equal(1006028374637854700);
+			expect(row.c).to.equal('1006028374637854687');
+			if (++count % 2) {
+				expect(row.a).to.equal(1006028374637854700);
+			} else {
+				expect(row.a).to.deep.equal(int);
+			}
+			stmt.safeIntegers(count % 2 ? true : false);
+		});
+		expect(count).to.equal(4);
+	});
+	it('should be safe from other databases inside an .each() callback', function () {
+		var int = new Int64(4243423, 234234234);
+		var stmt = db.prepare('SELECT a FROM entries').safeIntegers();
+		var stmt2 = db2.prepare('SELECT a FROM entries');
+		var count = 0;
+		stmt.each(function (row) {
+			++count;
+			expect(row.a).to.deep.equal(int);
+			
+			var subcount = 0;
+			stmt2.safeIntegers(false).each(function (row) {
+				++subcount;
+				expect(row.a).to.equal(1006028374637854700);
+			});
+			expect(subcount).to.equal(4);
+			
+		});
+		expect(count).to.equal(4);
+	});
+	it('should be able to change the default setting on the database', function () {
+		db.defaultSafeIntegers(true);
+		var int = new Int64(4243423, 234234234);
+		
+		var stmt = db.prepare('SELECT a FROM entries').pluck();
+		expect(stmt.get()).to.deep.equal(int);
+		expect(stmt.safeIntegers(false).get()).to.equal(1006028374637854700);
+		
+		db.defaultSafeIntegers(false);
+		
+		var stmt2 = db.prepare('SELECT a FROM entries').pluck();
+		expect(stmt2.get()).to.equal(1006028374637854700);
+		expect(stmt2.safeIntegers().get()).to.deep.equal(int);
+		
+		db.defaultSafeIntegers(true);
+		expect(stmt.get()).to.equal(1006028374637854700);
+		expect(stmt2.get()).to.deep.equal(int);
+	});
 });
