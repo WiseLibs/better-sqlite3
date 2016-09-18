1d86fe8d20429f03ec58592339857e7528492f0b
Joshua Wise <joshuathomaswise@gmail.com>
2016-09-17 23:20:51 -0400

    added pending Int64 tests

--- test/50.int64.js
+++ test/50.int64.js
@@ -2,10 +2,15 @@ var expect = require('chai').expect;
 var Database = require('../.');
 var Int64 = Database.Int64;
 var db;
+var db2;
 
 before(function (done) {
-	db = new Database('temp/' + require('path').basename(__filename).split('.')[0] + '.db');
-	db.on('open', done);
+	db = new Database('temp/' + require('path').basename(__filename).split('.')[0] + '.1.db');
+	db2 = new Database('temp/' + require('path').basename(__filename).split('.')[0] + '.2.db');
+	var openCount = 0;
+	db.on('open', opened);
+	db2.on('open', opened);
+	function opened() {++openCount === 2 && done();}
 });
 
 describe('Int64', function () {
@@ -19,6 +24,7 @@ describe('Int64', function () {
 	it('should compare to other Int64s or other values via .equals()');
 	it('should bind to statements and transactions');
 	it('should get returned by operations after setting .safeIntegers()');
+	it('should react to changing settings inside a .each() callback');
 	it('should be able to change the default setting on the database');
 	it('should return fully accurate round-trip results');
 });
