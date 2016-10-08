33efede5dc54d9eb70f782cd7ccb81df73dec7b5
Joshua Wise <joshuathomaswise@gmail.com>
2016-10-08 19:57:04 -0400

    fixed issue in install script for windows

--- install/index.js
+++ install/index.js
@@ -3,7 +3,8 @@ var args = process.env.CI === 'true' ? ['rebuild', '--debug'] : ['rebuild'];
 var child = require('child_process').spawn('node-gyp', args, {
 	encoding: 'utf8',
 	stdio: 'inherit',
-	cwd: require('path').dirname(__dirname)
+	cwd: require('path').dirname(__dirname),
+	shell: true
 });
 
 child.on('exit', function (code) {
