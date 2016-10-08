18754e3be4b3423d11c830ff97c556a92d67f9f9
Joshua Wise <joshuathomaswise@gmail.com>
2016-10-08 15:57:56 -0400

    attempt at making install script work on windows

---
+++ install/index.js
@@ -0,0 +1,12 @@
+'use strict';
+var args = process.env.CI === 'true' ? ['rebuild', '--debug'] : ['rebuild'];
+var child = require('child_process').spawn('node-gyp', args, {
+	encoding: 'utf8',
+	stdio: 'inherit',
+	cwd: require('path').dirname(__dirname)
+});
+
+child.on('exit', function (code) {
+	process.exit(code);
+});
+
