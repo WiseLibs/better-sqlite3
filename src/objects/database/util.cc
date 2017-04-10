3bafa2a7559ccfb183a6172174bbe222d30075ef
Joshua Wise <JoshuaWise@users.noreply.github.com>
2017-04-10 14:58:12 -0400

    whitespace

--- src/objects/database/util.cc
+++ src/objects/database/util.cc
@@ -1,13 +1,13 @@
 // .defaultSafeIntegers(boolean) -> this
 NAN_METHOD(Database::DefaultSafeIntegers) {
 	Database* db = Nan::ObjectWrap::Unwrap<Database>(info.This());
-
+	
 	if (info.Length() == 0 || info[0]->BooleanValue() == true) {
 		db->safe_ints = true;
 	} else {
 		db->safe_ints = false;
 	}
-
+	
 	info.GetReturnValue().Set(info.This());
 }
 
