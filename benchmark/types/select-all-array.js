2657356b9fa25e9628007fa790b58ebce80b62f0
Christophe Eymard <christophe.eymard@gmail.com>
2018-11-01 15:00:04 +0100

    added a few benchmarks for the array methods

---
+++ benchmark/types/select-all-array.js
@@ -0,0 +1,15 @@
+'use strict';
+exports.readonly = true; // Reading 100 rows into an array (`.all()`)
+
+exports['better-sqlite3'] = (db, { table, columns, count }) => {
+	const stmt = db.prepare(`SELECT ${columns.join(', ')} FROM ${table} WHERE rowid >= ? LIMIT 100`);
+  stmt.asArray();
+	let rowid = -100;
+	return () => stmt.all((rowid += 100) % count + 1);
+};
+
+exports['node-sqlite3'] = async (db, { table, columns, count }) => {
+	const sql = `SELECT ${columns.join(', ')} FROM ${table} WHERE rowid >= ? LIMIT 100`;
+	let rowid = -100;
+	return () => db.all(sql, (rowid += 100) % count + 1);
+};
---
+++ benchmark/types/select-array.js
@@ -0,0 +1,15 @@
+'use strict';
+exports.readonly = true; // Reading rows individually (`.get()`)
+
+exports['better-sqlite3'] = (db, { table, columns, count }) => {
+	const stmt = db.prepare(`SELECT ${columns.join(', ')} FROM ${table} WHERE rowid = ?`);
+  stmt.asArray();
+	let rowid = -1;
+	return () => stmt.get(++rowid % count + 1);
+};
+
+exports['node-sqlite3'] = async (db, { table, columns, count }) => {
+	const sql = `SELECT ${columns.join(', ')} FROM ${table} WHERE rowid = ?`;
+	let rowid = -1;
+	return () => db.get(sql, ++rowid % count + 1);
+};
