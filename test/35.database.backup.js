62758449a2f6c0ec2b0fd426d7f842089f75fce5
Joshua Thomas Wise <joshua.thomaswise@vice.com>
2019-01-17 16:58:33 -0500

    started writing tests for db.backup()

---
+++ test/35.database.backup.js
@@ -0,0 +1,13 @@
+'use strict';
+const Database = require('../.');
+
+describe('Database#backup()', function () {
+  beforeEach(function () {
+    this.db = new Database(util.next());
+  });
+  afterEach(function () {
+    this.db.close();
+  });
+
+
+});
