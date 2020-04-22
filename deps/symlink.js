551c23d28611cc96204174ed3f6eaec3e5089cdb
Joshua Wise <joshuathomaswise@gmail.com>
2020-04-22 15:14:17 -0500

    try supporting relative paths for --sqlite3 option

--- deps/symlink.js
+++ deps/symlink.js
@@ -3,11 +3,11 @@ const path = require('path');
 const fs = require('fs');
 
 const dest = process.argv[2];
-const source = path.resolve(path.sep, process.argv[3]);
+const source = path.resolve(process.argv[3]);
 
 /*
 	This creates symlinks inside the <$2> directory, linking to the SQLite3
-	amalgamation files inside the directory specified by the absolute path <$3>.
+	amalgamation files inside the directory specified by the path <$3>.
  */
 
 for (const filename of ['sqlite3.c', 'sqlite3.h']) {
