9f6050a5620ffaaa032846e5daf558777db2a95b
Joshua Wise <joshuathomaswise@gmail.com>
2022-01-18 17:45:57 -0600

    implemented "nativeBinding" database option

--- test/10.database.open.js
+++ test/10.database.open.js
@@ -1,5 +1,6 @@
 'use strict';
-const { existsSync } = require('fs');
+const fs = require('fs');
+const path = require('path');
 const Database = require('../.');
 
 describe('new Database()', function () {
@@ -31,10 +32,10 @@ describe('new Database()', function () {
 			expect(db.readonly).to.be.false;
 			expect(db.open).to.be.true;
 			expect(db.inTransaction).to.be.false;
-			expect(existsSync('')).to.be.false;
-			expect(existsSync('null')).to.be.false;
-			expect(existsSync('undefined')).to.be.false;
-			expect(existsSync('[object Object]')).to.be.false;
+			expect(fs.existsSync('')).to.be.false;
+			expect(fs.existsSync('null')).to.be.false;
+			expect(fs.existsSync('undefined')).to.be.false;
+			expect(fs.existsSync('[object Object]')).to.be.false;
 			db.close();
 		}
 	});
@@ -45,49 +46,49 @@ describe('new Database()', function () {
 		expect(db.readonly).to.be.false;
 		expect(db.open).to.be.true;
 		expect(db.inTransaction).to.be.false;
-		expect(existsSync(':memory:')).to.be.false;
+		expect(fs.existsSync(':memory:')).to.be.false;
 	});
 	it('should allow disk-bound databases to be created', function () {
-		expect(existsSync(util.next())).to.be.false;
+		expect(fs.existsSync(util.next())).to.be.false;
 		const db = this.db = new Database(util.current());
 		expect(db.name).to.equal(util.current());
 		expect(db.memory).to.be.false;
 		expect(db.readonly).to.be.false;
 		expect(db.open).to.be.true;
 		expect(db.inTransaction).to.be.false;
-		expect(existsSync(util.current())).to.be.true;
+		expect(fs.existsSync(util.current())).to.be.true;
 	});
 	it('should allow readonly database connections to be created', function () {
-		expect(existsSync(util.next())).to.be.false;
+		expect(fs.existsSync(util.next())).to.be.false;
 		expect(() => (this.db = new Database(util.current(), { readonly: true }))).to.throw(Database.SqliteError).with.property('code', 'SQLITE_CANTOPEN');
 		(new Database(util.current())).close();
-		expect(existsSync(util.current())).to.be.true;
+		expect(fs.existsSync(util.current())).to.be.true;
 		const db = this.db = new Database(util.current(), { readonly: true });
 		expect(db.name).to.equal(util.current());
 		expect(db.memory).to.be.false;
 		expect(db.readonly).to.be.true;
 		expect(db.open).to.be.true;
 		expect(db.inTransaction).to.be.false;
-		expect(existsSync(util.current())).to.be.true;
+		expect(fs.existsSync(util.current())).to.be.true;
 	});
 	it('should not allow the "readonly" option for in-memory databases', function () {
-		expect(existsSync(util.next())).to.be.false;
+		expect(fs.existsSync(util.next())).to.be.false;
 		expect(() => (this.db = new Database(':memory:', { readonly: true }))).to.throw(TypeError);
 		expect(() => (this.db = new Database('', { readonly: true }))).to.throw(TypeError);
-		expect(existsSync(util.current())).to.be.false;
+		expect(fs.existsSync(util.current())).to.be.false;
 	});
 	it('should accept the "fileMustExist" option', function () {
-		expect(existsSync(util.next())).to.be.false;
+		expect(fs.existsSync(util.next())).to.be.false;
 		expect(() => (this.db = new Database(util.current(), { fileMustExist: true }))).to.throw(Database.SqliteError).with.property('code', 'SQLITE_CANTOPEN');
 		(new Database(util.current())).close();
-		expect(existsSync(util.current())).to.be.true;
+		expect(fs.existsSync(util.current())).to.be.true;
 		const db = this.db = new Database(util.current(), { fileMustExist: true });
 		expect(db.name).to.equal(util.current());
 		expect(db.memory).to.be.false;
 		expect(db.readonly).to.be.false;
 		expect(db.open).to.be.true;
 		expect(db.inTransaction).to.be.false;
-		expect(existsSync(util.current())).to.be.true;
+		expect(fs.existsSync(util.current())).to.be.true;
 	});
 	util.itUnix('should accept the "timeout" option', function () {
 		this.slow(4000);
@@ -115,12 +116,38 @@ describe('new Database()', function () {
 		expect(() => (this.db = new Database(util.current(), { timeout: 75.01 }))).to.throw(TypeError);
 		expect(() => (this.db = new Database(util.current(), { timeout: 0x80000000 }))).to.throw(RangeError);
 	});
+	it('should accept the "nativeBinding" option', function () {
+		this.slow(500);
+		const oldBinding = require('bindings')({ bindings: 'better_sqlite3.node', path: true });
+		const newBinding = path.join(__dirname, '..', 'temp', 'foo.node');
+		expect(oldBinding).to.be.a('string');
+		fs.copyFileSync(oldBinding, newBinding);
+		const getBinding = db => db[Object.getOwnPropertySymbols(db)[0]].constructor;
+		let db1;
+		let db2;
+		let db3;
+		try {
+			db1 = new Database('');
+			db2 = new Database('', { nativeBinding: oldBinding });
+			db3 = new Database('', { nativeBinding: newBinding });
+			expect(db1.open).to.be.true;
+			expect(db2.open).to.be.true;
+			expect(db3.open).to.be.true;
+			expect(getBinding(db1)).to.equal(getBinding(db2));
+			expect(getBinding(db1)).to.not.equal(getBinding(db3));
+			expect(getBinding(db2)).to.not.equal(getBinding(db3));
+		} finally {
+			if (db1) db1.close();
+			if (db2) db2.close();
+			if (db3) db3.close();
+		}
+	});
 	it('should throw an Error if the directory does not exist', function () {
-		expect(existsSync(util.next())).to.be.false;
+		expect(fs.existsSync(util.next())).to.be.false;
 		const filepath = `temp/nonexistent/abcfoobar123/${util.current()}`;
 		expect(() => (this.db = new Database(filepath))).to.throw(TypeError);
-		expect(existsSync(filepath)).to.be.false;
-		expect(existsSync(util.current())).to.be.false;
+		expect(fs.existsSync(filepath)).to.be.false;
+		expect(fs.existsSync(util.current())).to.be.false;
 	});
 	it('should have a proper prototype chain', function () {
 		const db = this.db = new Database(util.next());
