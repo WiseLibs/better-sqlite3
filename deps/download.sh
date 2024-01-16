5a86096d821a1ce4ffd58c365db8558886ef951a
Joshua Wise <8255757+JoshuaWise@users.noreply.github.com>
2024-01-16 17:43:02 -0600

    Update SQLite to version 3.45.0 (#1130)
    Co-authored-by: mceachen <mceachen@users.noreply.github.com>

--- deps/download.sh
+++ deps/download.sh
@@ -18,8 +18,8 @@
 # 4. node-gyp links the two resulting binaries to generate better_sqlite3.node.
 # ===
 
-YEAR="2023"
-VERSION="3440200"
+YEAR="2024"
+VERSION="3450000"
 
 # Defines below are sorted alphabetically
 DEFINES="
