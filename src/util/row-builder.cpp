896573e3f5c65b88c6c5e01cd040c353e4bfb80c
neoxpert <18628980+neoxpert@users.noreply.github.com>
2025-10-28 18:01:22 +0100

    use GetPrototypeV2 on v8 API version 13+

--- src/util/row-builder.cpp
+++ src/util/row-builder.cpp
@@ -33,7 +33,11 @@ public:
 		}
 
 		return v8::Object::New(isolate,
+			#ifdef USE_GETPROTOTYPEV2
+			v8::Object::New(isolate)->GetPrototypeV2(),
+			#else
 			v8::Object::New(isolate)->GetPrototype(),
+			#endif
 			keys.data(),
 			values.data(),
 			column_count
